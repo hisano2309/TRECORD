02ba993cfecb13fd117a5b695ce852ddcfcc9503
Aramaki <Aramaki@sample.com>
2024-01-29 12:01:27 +0900

    init

---
+++ src/main/java/com/example/app/TrecodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrecodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrecodeApplication.class, args);
+	}
+
+}
