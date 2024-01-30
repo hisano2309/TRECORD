c4d5356cea3d689d040e04d4b273b0b181d3e333
Aramaki <Aramaki@sample.com>
2024-01-31 00:10:01 +0900

    service編集（荒牧）

---
+++ src/main/java/com/example/app/service/MachineSetCountServiceImpl.java
@@ -0,0 +1,49 @@
+package com.example.app.service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.app.domain.MachineSetCount;
+import com.example.app.mapper.MachineMapper;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional(rollbackFor=Exception.class)
+@RequiredArgsConstructor
+public class MachineSetCountServiceImpl implements MachineSetCountService{
+
+	private final MachineMapper machineMapper;
+
+	@Override
+	public List<MachineSetCount> getSelectAll() throws Exception {
+		return machineMapper.selectAll();
+	}
+
+	@Override
+	public List<MachineSetCount> getMachineSetCountDay() throws Exception {
+		return machineMapper.selectDay();
+	}
+
+	@Override
+	public void addMachineSetCount(MachineSetCount machineRecord) throws Exception {
+		machineMapper.insert(machineRecord);
+
+	}
+
+	@Override
+	public void editMachineSetCount(MachineSetCount machineRecord) throws Exception {
+		machineMapper.update(machineRecord);
+
+	}
+
+	@Override
+	public void deleteMachineSetCount(LocalDateTime date) throws Exception {
+		machineMapper.delete(date);
+
+	}
+
+}
