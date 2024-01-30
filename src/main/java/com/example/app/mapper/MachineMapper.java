f9ade2671562a373c10756566570f7bc28bb8a45
Aramaki <Aramaki@sample.com>
2024-01-30 11:06:36 +0900

    mapperクラス作成（荒牧）

---
+++ src/main/java/com/example/app/mapper/MachineMapper.java
@@ -0,0 +1,29 @@
+package com.example.app.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.app.domain.Machine;
+
+@Mapper
+public interface MachineMapper {
+	
+//表示//
+	//筋トレマシン一覧表示
+	List<Machine> selectAll() throws Exception;
+
+//登録//
+	//筋トレ（マシンあり）記録登録
+	void insert(Machine machineRecord) throws Exception;
+
+	
+//編集//
+	//筋トレ（マシンあり）記録編集
+	void update(Machine machineRecord) throws Exception;
+	
+//削除//
+	//筋トレ（マシンあり）記録削除
+	void delete(Machine machineRecord) throws Exception;
+
+}
