6c70174bb93122c2995c6b048f07db0e3880695d
Aramaki <Aramaki@sample.com>
2024-01-31 00:09:17 +0900

    domain編集（荒牧）

--- src/main/java/com/example/app/domain/MachineSetCount.java
+++ src/main/java/com/example/app/domain/MachineSetCount.java
@@ -5,16 +5,16 @@ import java.time.LocalDateTime;
 import lombok.Data;
 
 @Data
-public class SetCount {
+public class MachineSetCount {
 
 	private int userId;
 	private LocalDateTime date;
-	
-	private Integer machineId;	
+
+	private Integer machineId;
 	private Integer machineCount;
-	
+
 	//private Integer bodyWeightId;
 	//private Integer bodyWeightCount;
-	
+
 	private int sets;
 }
