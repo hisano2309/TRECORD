aad3295db0e0375aa0a71fbf64ce13e8f3a1aa8f
Aramaki <Aramaki@sample.com>
2024-01-31 00:10:41 +0900

    mapper編集（荒牧）

--- src/main/java/com/example/app/mapper/MachineMapper.java
+++ src/main/java/com/example/app/mapper/MachineMapper.java
@@ -1,29 +1,33 @@
 package com.example.app.mapper;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
-import com.example.app.domain.Machine;
+import com.example.app.domain.MachineSetCount;
 
 @Mapper
 public interface MachineMapper {
-	
+
 //表示//
-	//筋トレマシン一覧表示
-	List<Machine> selectAll() throws Exception;
+	//筋トレ記録全表示
+	List<MachineSetCount> selectAll() throws Exception;
+
+	//カレンダーから特定の日の筋トレ記録表示
+	List<MachineSetCount> selectDay() throws Exception;
 
 //登録//
 	//筋トレ記録登録
-	void insert(Machine machineRecord) throws Exception;
+	void insert(MachineSetCount machineRecord) throws Exception;
 
-	
+
 //編集//
 	//筋トレ記録編集
-	void update(Machine machineRecord) throws Exception;
-	
+	void update(MachineSetCount machineRecord) throws Exception;
+
 //削除//
 	//筋トレ記録削除
-	void delete(Machine machineRecord) throws Exception;
+	void delete(LocalDateTime date) throws Exception;
 
 }
