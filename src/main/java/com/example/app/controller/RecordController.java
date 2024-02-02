2f9e6712037da466380fe520ab287dd50c1a2356
Yui Ogushi <ogushi.yui0531@gmail.com>
2024-02-02 10:53:42 +0900

    画像　更新機能修正 ogushi

--- src/main/java/com/example/app/controller/RecordController.java
+++ src/main/java/com/example/app/controller/RecordController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,19 +17,21 @@ import org.springframework.web.multipart.MultipartFile;
 import com.example.app.domain.Image;
 import com.example.app.mapper.RecordMapper;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/mypage")
+@RequestMapping
 public class RecordController {
 
 	private final RecordMapper mapper;
 
 	// ユーザーの画像一覧取得
-	@GetMapping
+	@GetMapping("/mypage")
 	public String list(Model model) {
 		List<Image> imgList = mapper.getImageByUserId(1);
+		System.out.println(imgList);
 		model.addAttribute("imgList", imgList);
 		return "mypage";
 	}
@@ -61,7 +64,7 @@ public class RecordController {
 			throws IllegalStateException, IOException {
 		if (upload.isEmpty()) {
 			model.addAttribute("msg", "画像が選択されていません");
-			return "redirect:/record";
+			return "record";
 
 		} else {
 			// ファイルサイズ
@@ -73,7 +76,7 @@ public class RecordController {
 			// ファイル名取得
 			String imgName = upload.getOriginalFilename();
 			// 格納場所取得(各々のフォルダ名に変更して下さい)
-			File dest = new File("C:/Users/uploads/" + imgName);
+			File dest = new File("C:/Users/zd3M06/uploads/" + imgName);
 			// File dest = new File("C:/uploads/" + imgName);
 			Image image = new Image();
 			image.setImgName(imgName);
@@ -82,9 +85,10 @@ public class RecordController {
 			mapper.add(image);// DBに保存
 
 			// TODO ここにレベルアップ機能を実装
+			// TODO 済み画像表示機能実装
 
-			return "redirect:/mypage";
 		}
+		return "redirect:/mypage";
 	}
 
 	// トレーニング内容個別編集
@@ -96,13 +100,16 @@ public class RecordController {
 	}
 
 	@PostMapping("/edit/{id}")
-	public String edit(@RequestParam MultipartFile upload, Image image,@PathVariable Integer id, Model model) throws IllegalStateException, IOException {
-//		if (errors.hasErrors()) {
-//			System.out.println("errors");
-//			model.addAttribute("image", image);
-//			return "record";
-//		}
-		
+	public String edit(
+			@RequestParam MultipartFile upload,
+			@Valid Image image, Errors errors, 
+			@PathVariable Integer id,
+			Model model) throws IllegalStateException, IOException {
+		if (errors.hasErrors()) {
+			System.out.println("errors");
+			model.addAttribute("image", image);
+			return "record";
+		}
 		String imgName = upload.getOriginalFilename();
 		image.setImgId(id);
 		image.setImgName(imgName);
