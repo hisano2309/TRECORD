94a4ae4eb296c9ed38890875a71ca0934be3ba8c
Aramaki <Aramaki@sample.com>
2024-02-02 16:16:58 +0900

    体重記録機能追加（荒牧）

---
+++ src/main/java/com/example/app/service/WeightService.java
@@ -0,0 +1,21 @@
+package com.example.app.service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import com.example.app.domain.Weight;
+
+public interface WeightService {
+
+//表示
+	//体重表示
+	List<Weight> getSelectWeight(Weight userId, LocalDate date) throws Exception;	
+
+//登録
+	//体重の登録
+	void insertWeight(Weight userWeight) throws Exception;
+
+	//BMIの登録
+	void insertBmi(Weight bmi) throws Exception;
+
+}
