026a3eaf082180487e703f56ee59e170b5c95fab
Aramaki <Aramaki@sample.com>
2024-02-05 12:06:39 +0900

    体重の記録、表示（荒牧）

--- src/main/java/com/example/app/controller/WeightController.java
+++ src/main/java/com/example/app/controller/WeightController.java
@@ -1,32 +1,82 @@
 package com.example.app.controller;
 
+import java.time.LocalDate;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.app.domain.WeightBmi;
 import com.example.app.service.WeightService;
 
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
 public class WeightController {
 
-	private final WeightService Service;
+	private final WeightService service;
 
 //登録
 	//体重の登録
 	@GetMapping("/weightRegister")
 	public String weightRegister(Model model) throws Exception{
-
+		WeightBmi weightBmi = new WeightBmi();
+		//日付
+		LocalDate now = LocalDate.now();
+		weightBmi.setDate(now);
+		weightBmi.setUserWeight(50.5);
+		model.addAttribute("weightBmi", weightBmi);
 		return "weight";
 	}
 
-	//BMIの登録
+	//体重、BMIの登録
 	@PostMapping("/weightRegister")
-	public String bmiRegister(Model model) throws Exception{
+	public String bmiRegister(
+			Model model,
+			@Valid WeightBmi weightBmi,
+			Errors errors,
+			HttpSession session) throws Exception{
+		
 
+		//!!!!!!!!!!!ダミーデータ!!!!!!!!!!!!!!!!!!!!!!!!
+		weightBmi.setUserId(1);
+//		User user = (User) session.getAttribute("user");
+//		weightBmi.setUserId(user.getUserId());
+		
+
+		//BMIを計算
+			//BMI ＝ 体重kg ÷ (身長m)2
+			//適正体重 ＝ (身長m)2 ×22
+		
+		//!!!!!!!!!!!ダミーデータ!!!!!!!!!!!!!!!!!!!!!!!!
+		double heightCm = 160;
+//		double heightCm = user.getUserHeight();   //単位：cm
+		
+		double userHeight = heightCm / 100;   //単位：m
+		double bmi = weightBmi.getUserWeight() / (userHeight * userHeight);
+		double healthyWeight = (userHeight*userHeight) * 22;
+		
+		//小数点第二位で切り捨て
+		weightBmi.setBmi(Math.floor(bmi * 10) / 10);
+		weightBmi.setHealthyWeight(Math.floor(healthyWeight * 10) / 10);
+		
+		//体重・BMIの登録
+		service.insertWeightBmi(weightBmi);
+		
+		model.addAttribute("weightBmi", weightBmi);
+		
+//		System.out.println("weightBmiの中身" + weightBmi.getWeightId());
+//		System.out.println("weightBmiの中身" + weightBmi.getUserId());
+//		System.out.println("weightBmiの中身" + weightBmi.getUserWeight());
+//		System.out.println("weightBmiの中身" + weightBmi.getBmi());
+//		System.out.println("weightBmiの中身" + weightBmi.getHealthyWeight());
+//		System.out.println("weightBmiの中身" + weightBmi.getDate());
+		
 		return "weightRegisterDone";
 	}
 
--- src/main/java/com/example/app/mapper/WeightMapper.java
+++ src/main/java/com/example/app/mapper/WeightMapper.java
@@ -5,23 +5,23 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
-import com.example.app.domain.Weight;
+import com.example.app.domain.WeightBmi;
 
 @Mapper
 public interface WeightMapper {
 
 //表示
 	//体重表示
-	List<Weight> selectWeight(Weight userId, LocalDate date) throws Exception;
+	List<WeightBmi> selectWeight(WeightBmi userId, LocalDate date) throws Exception;
 	//BMI表示
 //	List<Weight> selectBmi(Weight userWeight, User height) throws Exception;
 
 //登録
 	//体重の登録
-	void insertWeight(Weight userWeight) throws Exception;
+	void insertWeightBmi(WeightBmi WeightBmi) throws Exception;
 
 	//BMIの登録
-	void insertBmi(Weight bmi) throws Exception;
+//	void insertBmi(WeightBmi bmi) throws Exception;
 
 
 }
