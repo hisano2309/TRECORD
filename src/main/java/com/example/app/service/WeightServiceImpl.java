425fe825b2ce07cd9bf69b132520e01ebb9723c0
Aramaki <Aramaki@sample.com>
2024-02-05 22:58:48 +0900

    体重・BMIの表示（荒牧）

--- src/main/java/com/example/app/service/WeightServiceImpl.java
+++ src/main/java/com/example/app/service/WeightServiceImpl.java
@@ -1,5 +1,8 @@
 package com.example.app.service;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,25 +15,18 @@ import lombok.RequiredArgsConstructor;
 @Transactional(rollbackFor=Exception.class)
 @RequiredArgsConstructor
 public class WeightServiceImpl implements WeightService{
-	
+
 	private final WeightMapper weightMapper;
-
-//	@Override
-//	public List<Weight> getSelectWeight(Weight userId, LocalDate date) throws Exception {
-//		// TODO 自動生成されたメソッド・スタブ
-//		return null;
-//	}
-
-//	@Override
-//	public void insertBmi(WeightBmi bmi) throws Exception {
-//		weightMapper.insertBmi( bmi);
-//		
-//	}
 
 	@Override
 	public void insertWeightBmi(WeightBmi WeightBmi) throws Exception {
 		weightMapper.insertWeightBmi(WeightBmi);
 
+	}
+
+	@Override
+	public List<WeightBmi> getSelectBeforeWeightbmi(int userId, LocalDate date) throws Exception {
+		return weightMapper.selectBeforeWeightBmi(userId, date);
 	}
 
 
