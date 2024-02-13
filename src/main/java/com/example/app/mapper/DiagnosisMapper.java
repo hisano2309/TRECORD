0dba5b8a571d7e8c084a583f12fb8b640fe39ba2
Aramaki <Aramaki@sample.com>
2024-02-13 15:43:36 +0900

    診断機能domain

---
+++ src/main/java/com/example/app/mapper/DiagnosisMapper.java
@@ -0,0 +1,10 @@
+package com.example.app.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.transaction.annotation.Transactional;
+
+@Mapper
+@Transactional
+public interface DiagnosisMapper {
+
+}
