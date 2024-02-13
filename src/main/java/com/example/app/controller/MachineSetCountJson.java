e387984d95505de30d6f77f5aff5eb5ede9882c1
Aramaki <Aramaki@sample.com>
2024-02-13 14:40:35 +0900

    円グラフ表示（荒牧）

--- src/main/java/com/example/app/controller/MachineSetCountJson.java
+++ src/main/java/com/example/app/controller/MachineSetCountJson.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.app.domain.Count;
 import com.example.app.domain.MachineSetCount;
 import com.example.app.service.MachineSetCountService;
 
@@ -18,66 +19,55 @@ public class MachineSetCountJson {
 
 	private final MachineSetCountService service;
 	
-	@GetMapping("/MachineSetCountJson")
-	public List<List<MachineSetCount>> list(HttpSession session) throws Exception{
+	@GetMapping("/MachineSetCountLineGraph")
+	public List<List<MachineSetCount>> line(HttpSession session) throws Exception{
 		
 		//!!!!!!!!!!!ダミーデータ!!!!!!!!!!!!!!!!!!!!!!!!
 		MachineSetCount machineSetCount = new MachineSetCount();
 		machineSetCount.setUserId(1);
 		
 		
-//		List<Machine> machine = new ArrayList<>();
-//		int size = machine.size();
-//		for(int i = 1; i <= size; i++) {
-//			machineSetCount.setMachineId(i);
-//			return service.getSelectChartMachineId1(machineSetCount.getUserId(), machineSetCount.getMachineId());
-//		}
-//		System.out.println("size" + size);
-//		return null;
-		
-//		int countMachine = service.getCountMachine();
-//		int i = 1;
-//		while(i <= countMachine) {
-//			machineSetCount.setMachineId(i);
-//			return service.getSelectChartMachineId1(machineSetCount.getUserId(), machineSetCount.getMachineId());
-//		}
-//		return null;
-		
-//		int countMachine = service.getCountMachine();
-//		for(int i = 1; i <= countMachine; i++) {
-//			if(i == 1) return service.getSelectChartMachineId1(machineSetCount.getUserId(), machineSetCount.getMachineId());
-//			if(i == 2) return service.getSelectChartMachineId2(machineSetCount.getUserId(), machineSetCount.getMachineId());
-//			if(i == 3) return service.getSelectChartMachineId3(machineSetCount.getUserId(), machineSetCount.getMachineId());
-//			if(i == 4) return service.getSelectChartMachineId4(machineSetCount.getUserId(), machineSetCount.getMachineId());
-//			if(i == 5) return service.getSelectChartMachineId5(machineSetCount.getUserId(), machineSetCount.getMachineId());
-//			if(i == 6) return service.getSelectChartMachineId6(machineSetCount.getUserId(), machineSetCount.getMachineId());
-//		}
-//		return null;
-		
 		machineSetCount.setMachineId(1);
-		List<MachineSetCount> machine1 = service.getSelectChartMachineId1(machineSetCount.getUserId(), machineSetCount.getMachineId());
+		List<MachineSetCount> machine1 = service.getSelectLineGraph(machineSetCount.getUserId(), machineSetCount.getMachineId());
 		machineSetCount.setMachineId(2);
-		List<MachineSetCount> machine2 = service.getSelectChartMachineId1(machineSetCount.getUserId(), machineSetCount.getMachineId());
+		List<MachineSetCount> machine2 = service.getSelectLineGraph(machineSetCount.getUserId(), machineSetCount.getMachineId());
 		machineSetCount.setMachineId(3);
-		List<MachineSetCount> machine3 = service.getSelectChartMachineId1(machineSetCount.getUserId(), machineSetCount.getMachineId());
+		List<MachineSetCount> machine3 = service.getSelectLineGraph(machineSetCount.getUserId(), machineSetCount.getMachineId());
+		machineSetCount.setMachineId(4);
+		List<MachineSetCount> machine4 = service.getSelectLineGraph(machineSetCount.getUserId(), machineSetCount.getMachineId());
+		machineSetCount.setMachineId(5);
+		List<MachineSetCount> machine5 = service.getSelectLineGraph(machineSetCount.getUserId(), machineSetCount.getMachineId());
+		machineSetCount.setMachineId(6);
+		List<MachineSetCount> machine6 = service.getSelectLineGraph(machineSetCount.getUserId(), machineSetCount.getMachineId());
 		
 		List<List<MachineSetCount>> machineList = new ArrayList<>();
 		machineList.add(machine1);
 		machineList.add(machine2);
 		machineList.add(machine3);
+		machineList.add(machine4);
+		machineList.add(machine5);
+		machineList.add(machine6);
 		System.out.println("machineList->"+machineList);
 //		return service.getSelectChartMachineId1(machineSetCount.getUserId(), machineSetCount.getMachineId());
 		return machineList;
 	}
 	
 	
-//	public List<MachineSetCount> list(HttpSession session) throws Exception{
-//		
-//		//!!!!!!!!!!!ダミーデータ!!!!!!!!!!!!!!!!!!!!!!!!
-//		WeightBmi weightBmi = new WeightBmi();
-//		weightBmi.setUserId(1);
-//		
-//		return service.getSelectChart(weightBmi.getUserId());
-//	}
+	@GetMapping("/MachineSetCountPieGraph")
+	public List<Count> pie(HttpSession session) throws Exception{
+		
+		//!!!!!!!!!!!ダミーデータ!!!!!!!!!!!!!!!!!!!!!!!!
+		MachineSetCount machineSetCount = new MachineSetCount();
+		machineSetCount.setUserId(1);
+		
+		// System.out.println("machineSetCount.getUserId()->"+machineSetCount.getUserId());
+		
+		List<Count> count = service.getSelectPieGraph(machineSetCount.getUserId());
+		// System.out.println("service.getSelectPieGraph" + service.getSelectPieGraph(machineSetCount.getUserId()));
+		// System.out.println("count" + count);
+		
+		return count;
+	}
+
 	
 }
---
+++ src/main/java/com/example/app/domain/Count.java
@@ -0,0 +1,11 @@
+package com.example.app.domain;
+
+import lombok.Data;
+
+@Data
+public class Count {
+
+	private int machineId;
+	private String machineName;
+	private int count;
+}
