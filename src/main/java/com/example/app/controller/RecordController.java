8260108066f362fb281ae967c2018624aaa94581
Aramaki <Aramaki@sample.com>
2024-02-17 01:20:53 +0900

    RecordController(途中)

--- src/main/java/com/example/app/controller/RecordController.java
+++ src/main/java/com/example/app/controller/RecordController.java
@@ -1,17 +1,24 @@
 package com.example.app.controller;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.app.domain.Image;
 import com.example.app.domain.Machine;
 import com.example.app.domain.MachineSetCount;
+import com.example.app.domain.User;
+import com.example.app.mapper.ImageMapper;
 import com.example.app.service.MachineSetCountService;
+
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -21,13 +28,35 @@ import lombok.RequiredArgsConstructor;
 public class RecordController {
 	// 筋トレ記録
 	private final MachineSetCountService service;
+	// 画像
+	private final ImageMapper mapper;
+	// 1ページあたりの表示件数
+	private static final int NUM_PER_PAGE = 6;
 
 
 
-// 【ユーザーの画像一覧取得】
-	@GetMapping("/mypage")
-	public String list(Model model,
-			HttpSession session) {
+@GetMapping("/mypage")
+	public String list(@RequestParam(name = "page", defaultValue = "1") Integer page, Model model,
+		HttpSession session) throws Exception {
+	// ユーザーの画像一覧取得
+		User user = (User) session.getAttribute("user");
+		List<Image> imgList = mapper.getImageByUserId(user.getUserId());
+		System.out.println(imgList);
+		// ページング追加のため以下の記述をコメントアウト
+		// model.addAttribute("imgList", imgList);
+		int offset = NUM_PER_PAGE * (page - 1);
+		model.addAttribute("imgList", mapper.selectLimited(offset, NUM_PER_PAGE));
+
+		// 現在のページを更新
+		model.addAttribute("page", page);
+
+		// 全体のデータ数取得
+		// 小数点で受け取りたいため、キャスト
+		double totalNum = (double) mapper.count();
+		// 全体のページ数の計算：numPerPageは1ページあたりの表示件数とする
+		// Math.ceil 小数点以下を切り上げしてくれる
+		int totalPage = (int) Math.ceil((double) totalNum / NUM_PER_PAGE);
+		model.addAttribute("totalPage", totalPage);
 
 		return "mypage";
 	}
@@ -45,6 +74,7 @@ public class RecordController {
 		machineSetCount.setUserId(1);
 
 		//日付
+		//!!!!!!!!!!!ダミーデータ!!!!!!!!!!!!!!!!!!!!!!!!
 		LocalDate now = LocalDate.now();
 		machineSetCount.setDate(now);
 		System.out.println("今日の日付：" + machineSetCount.getDate());
@@ -81,6 +111,46 @@ public class RecordController {
 			service.addMachineSetCount(machineSetCount);
 
 		return "recordDone";
+	}
+
+
+// 【トレーニング記録詳細ページ】
+	// "/show/{date}"→アルバムからshow、"/show"→カレンダーからshow
+	@GetMapping({"/show/{date}", "/show"})
+	public String showDay(
+			@PathVariable(required = false) String date, // アルバムをクリックdate
+			@RequestParam(name = "date2", required = false) String date2, // カレンダーのパスから引っ張ってくるdate
+			HttpSession session,
+			Model model) throws Exception {
+
+	// 画像の個別取得
+		User user = (User) session.getAttribute("user");
+
+      // 個別でデータをとる記述
+		List<Image> image  = null;
+		if(date != null) {
+		// アルバムからshow("/show/{date}")
+			image = mapper.getImageByDate(user.getUserId(), date);
+		}else {
+		// カレンダーからshow("/show")
+			// 画像
+			image = mapper.getImageByDate
+					(user.getUserId(), date2);
+
+			//カレンダーから特定の日の筋トレ記録を取得
+			// トレーニング記録
+			MachineSetCount machineSetCount = new MachineSetCount();
+			List<MachineSetCount> getDayData = service.getMachineSetCountDay(date2, user.getUserId());
+			machineSetCount.setDate(date2);
+
+			System.out.println("getDayData：" + getDayData);
+			model.addAttribute("machineSetCount", getDayData);
+		}
+
+		System.out.println(image);
+		model.addAttribute("imageList", image);
+
+		return "show";
 	}
 
 
