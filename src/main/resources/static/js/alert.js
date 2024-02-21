da9c4161681e5aa13bb9314e21014fb1f16119be
Aramaki <Aramaki@sample.com>
2024-02-21 14:11:16 +0900

    alertを追加

--- src/main/resources/static/js/alert.js
+++ src/main/resources/static/js/alert.js
@@ -1,3 +1,16 @@
-document.getElementById("alertButton").onclick = function() {
-  alert("完了しました");
-};
+// document.getElementById("completion").onclick = function() {
+// 	alert("完了しました");
+// };
+
+// document.getElementById("removes").onclick = function() {
+//	alert("削除完了しました");
+// };
+
+
+ $('#completion').click(function(){
+	 alert('登録完了しました');
+ });
+ 
+ $('#remove').click(function(){
+	 alert('削除完了しました');
+ });
